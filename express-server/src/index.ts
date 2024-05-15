0658012d342dbf3f465f04dcb8f00b9593c2bb70
dpetrova <daniella_petrova@abv.bg>
2024-05-15 11:30:37 +0300

    some routes

--- express-server/src/index.ts
+++ express-server/src/index.ts
@@ -1,16 +1,17 @@
-import express, {Request, Response} from "express"
+import express from "express"
+import { router } from "./routes/login"
+import bodyParser from "body-parser"
+import cookieSession from "cookie-session"
 
 // create app
 const app = express()
 
+// middlewares
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieSession({keys: ['verysecretdummystring']}))
+
 // routes
-app.get('/', (req: Request, res: Response) => {
-    res.send(
-        `<div>
-            <h1>Ni there!</h1>
-        </div>`
-    )
-})
+app.use(router)
 
 app.listen(3000, () => {
     console.log("Listening on port 3000")
